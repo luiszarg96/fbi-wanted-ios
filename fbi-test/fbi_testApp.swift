829a3fcdc330b8a0650d57adb479f52ff91a7ff5
Luis Zarza <luis.zarza01@gmail.com>
2024-10-15 15:51:26 -0300

    Initial Commit

---
+++ fbi-test/fbi_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  fbi_testApp.swift
+//  fbi-test
+//
+//  Created by Luis Zarza on 2024-10-15.
+//
+
+import SwiftUI
+
+@main
+struct fbi_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
